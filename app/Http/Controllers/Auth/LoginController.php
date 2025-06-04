ff4f58431251909438c1249f78953f5c2b4d1df6
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-06-04 03:50:17 -0300

    feat: login user

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        try {
+            $data = $request->validated();
+            $user = User::where('email', $data['email'])->first();
+
+            if (! $user || ! Hash::check($data['password'], $user->password)) {
+                return new JsonResponse([
+                    'status' => 'error',
+                    'message' => "Invalid credentials"
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+
+            $token = $user->createToken('liberfly-' . $user->id . 'token')->plainTextToken;
+
+            return new JsonResponse([
+                'status' => 'success',
+                'data' => [
+                    'token' => $token,
+                    'expiration' => 525600,
+                ]
+            ], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return new JsonResponse([], Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => [Password::min(8)->letters()->mixedCase()->numbers()->symbols()->uncompromised()],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        return new JsonResponse([
+            'status' => 'error',
+            'message' => $validator->errors(),
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
