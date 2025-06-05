596d557cbc7942c6384f4fa07ff01bde4ac28adb
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-06-05 02:13:36 -0300

    feat: add new parameters

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -16,6 +16,14 @@ class LoginController extends Controller
     #[OA\Post(
         path: "api/auth/login",
         description: "User Login",
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\MediaType(
+                mediaType: "application/json",
+                example: "{\"email\":\"adrian.runolfsdottir51@yahoo.com\", \"password\":\"1*Kg=4J3p\"}",
+            )
+        ),
+        tags: ["auth"],
         responses: [
             new OA\Response(response: 200, description: "Successful login"),
             new OA\Response(response: 401, description: "Unauthorized"),
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -17,8 +17,17 @@ class RegisterController extends Controller
     #[OA\Post(
         path: "api/auth/register",
         description: "Register a new user",
+        summary: "Register a new user",
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\MediaType(
+                mediaType: "application/json",
+                example: "{\"name\":\"Benny Keebler\",\"email\":\"adrian.runolfsdottir51@yahoo.com\", \"password\":\"1*Kg=4J3p\", \"password_confirmation\":\"1*Kg=4J3p\"}"
+            ),
+        ),
+        tags: ["auth"],
         responses: [
-            new OA\Response(response: 201, description: "Successful register user"),
+            new OA\Response(response: 201 ,description: "Successful register user"),
             new OA\Response(response: 422, description: "Error when validating data"),
             new OA\Response(response: 400, description: "Generic or unmapped error"),
         ]
