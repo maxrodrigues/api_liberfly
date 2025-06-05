18d8ec37b3ef288f21787d52de8e3bf91b7e124c
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-06-05 01:07:07 -0300

    feat: add basic swagger doc

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserListResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
+use OpenApi\Attributes as OA;
 
 class RegisterController extends Controller
 {
+    #[OA\Post(
+        path: "api/auth/register",
+        description: "Register a new user",
+        responses: [
+            new OA\Response(response: 201, description: "Successful register user"),
+            new OA\Response(response: 422, description: "Error when validating data"),
+            new OA\Response(response: 400, description: "Generic or unmapped error"),
+        ]
+    )]
     public function __invoke(RegisterRequest $request): JsonResponse
     {
         try {
@@ -22,14 +34,21 @@ class RegisterController extends Controller
             return new JsonResponse([
                 'status' => 'success',
                 'data' => [
-                    'user' => $user,
+                    'user' => new UserListResource($user),
                     'token_type' => 'Bearer',
                     'token' => $token,
                     'expiration' => 525600,
                 ]
             ], Response::HTTP_CREATED);
         } catch (\Exception $e) {
-            return new JsonResponse([], Response::HTTP_BAD_REQUEST);
+            Log::critical("Failed to create user", [
+                self::class,
+                'message' => $e->getMessage(),
+            ]);
+            return new JsonResponse([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ], Response::HTTP_BAD_REQUEST);
         }
     }
 }
