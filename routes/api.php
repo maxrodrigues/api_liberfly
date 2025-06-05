6173e8e5e2358604dc7d037067dd0fc9f7464b0c
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-06-05 02:41:23 -0300

    feat: add route protection

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::get('users-list', \App\Http\Controllers\UsersListController::class)->name('users-list');
-Route::post('add-user', \App\Http\Controllers\AddUserController::class)->name('add-user');
-
-Route::get('user/{user}', \App\Http\Controllers\ListUserController::class)->name('get-user');
-
-
 Route::post('auth/login', \App\Http\Controllers\Auth\LoginController::class)->name('login');
 Route::post('auth/register', \App\Http\Controllers\Auth\RegisterController::class)->name('register');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('users-list', \App\Http\Controllers\UsersListController::class)->name('users-list');
+    Route::post('add-user', \App\Http\Controllers\AddUserController::class)->name('add-user');
+    Route::get('user/{user}', \App\Http\Controllers\ListUserController::class)->name('get-user');
+});
